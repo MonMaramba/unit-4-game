d43c7c4366636edfc2fcd63870705427974e340d
unknown <monmaramba@yahoo.com>
2018-06-11 03:12:56 -0700

    clean up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,26 +3,13 @@
 var wins = 0;
 var loss = 0;
 var runningTotal = 0;
-
+randomNum = 0;
 
 //create array that contains the crystals
 
 var crystalPoints = [];
 
 //create function for game reset
-
-function reset() {
-
-   var wins = wins;
-   var loss = loss;
-   crystalPoints.length = 0;
-   runningTotal = 0;
-   $('#currentTotal').text(runningTotal);
-   
-} 
-   
-
-
 
 //generate random number between 19 - 120 and display on randomNumber div
 
@@ -37,7 +24,7 @@ $('#randomNumber').text(randomNum);
 
     for (i=0; i < 4; i++) {
     var arNum = [Math.floor(Math.random() * 12) + 1];
-    if (crystalPoints.indexOf(arNum) < 0) {
+    if (crystalPoints.indexOf(arNum) == -1) {
     crystalPoints.push(arNum);
     } 
     console.log(crystalPoints);
@@ -46,24 +33,44 @@ $('#randomNumber').text(randomNum);
 
 function reset() {
 
-    var wins = wins;
-    var loss = loss;
+   var wins = wins;
+   var loss = loss;
+   crystalPoints.length = 0;
+   runningTotal = 0;
+
+   $('#currentTotal').text(runningTotal);
+    
     crystalPoints.length = 0; 
+    
     runningTotal = 0;
+    
     $('#currentTotal').text(runningTotal);
 
     var randomNum = [Math.floor(Math.random() * 101 + 19)];
     $('#randomNumber').text(randomNum);
 
     for (i=0; i < 4; i++) {
-        var arNum = [Math.floor(Math.random() * 12) + 1];
-        if (crystalPoints.indexOf(arNum) < 0) {
+        arNum = [Math.floor(Math.random() * 12) + 1];
+        if (crystalPoints.indexOf(arNum) == -1 ) {
         crystalPoints.push(arNum);
-        } 
-        console.log(crystalPoints);
+        
+       } 
+       console.log(crystalPoints);
+       console.log(runningTotal);
+       console.log(randomNum);
+          
     }
  } 
+
+ 
     
+ if (randomNum == runningTotal) {
+    //winner();
+    winner();
+} else if (runningTotal > randomNum) {
+    //loser()
+    loser();
+}
 
 //win and lose functions
 
@@ -84,6 +91,8 @@ function loser() {
         reset();
 }
 
+
+
 /*listen for click event and assign value to crystal
 clicked and add to total*/
 
@@ -93,6 +102,10 @@ $('#crystal1').on('click', function(){
     crystalPoints[0] = parseInt(crystalPoints[0]);
     runningTotal = runningTotal + (crystalPoints[0]);
     $('#currentTotal').text(runningTotal);
+
+    console.log(runningTotal);
+    console.log(randomNum);
+   
 
     if (randomNum == runningTotal) {
         //winner();
@@ -111,6 +124,9 @@ $('#crystal2').on('click', function(){
     runningTotal = runningTotal + (crystalPoints[1]);
     $('#currentTotal').text(runningTotal);
 
+    console.log(runningTotal);
+    console.log(randomNum);
+
     if (randomNum == runningTotal) {
         //winner();
         winner();
@@ -126,6 +142,9 @@ $('#crystal3').on('click', function(){
     crystalPoints[2] = parseInt(crystalPoints[2]);
     runningTotal = runningTotal + (crystalPoints[2]);
     $('#currentTotal').text(runningTotal);
+
+    console.log(runningTotal);
+    console.log(randomNum);
 
     if (randomNum == runningTotal) {
        //winner();
@@ -144,6 +163,9 @@ $('#crystal4').on('click', function(){
     runningTotal = runningTotal + (crystalPoints[3]);
     $('#currentTotal').text(runningTotal);
 
+    console.log(runningTotal);
+    console.log(randomNum);
+
     if (randomNum == runningTotal) {
         //winner();
         winner();
@@ -153,6 +175,5 @@ $('#crystal4').on('click', function(){
     }
 
 })
-
 
 
